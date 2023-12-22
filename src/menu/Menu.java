fd11d21ba570fb26cacc91412607bcdf69722ba9
Gabriel <gabriel.qmacedo@gmail.com>
2023-12-22 09:21:06 -0300

    menu e variaveis-lacos

---
+++ src/menu/Menu.java
@@ -0,0 +1,79 @@
+package menu;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		while (true) {
+
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                MERCADO PRESO                        ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Ver Carrinho                         ");
+			System.out.println("            2 - Colocar no Carrinho                  ");
+			System.out.println("            3 - Finalizar                            ");
+			System.out.println("            4 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     ");
+
+			opcao = leia.nextInt();
+
+			if (opcao == 5) {
+				System.out.println("\nMercado Preso - Seu Destino para Compras Confiáveis.");
+                 leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+				case 1:
+					System.out.println("Criar Conta\n\n");
+
+					break;
+				case 2:
+					System.out.println("Listar todas as Contas\n\n");
+
+					break;
+				case 3:
+					System.out.println("Consultar dados da Conta - por número\n\n");
+
+					break;
+				case 4:
+					System.out.println("Atualizar dados da Conta\n\n");
+
+					break;
+				case 5:
+					System.out.println("Apagar a Conta\n\n");
+
+					break;
+				case 6:
+					System.out.println("Saque\n\n");
+
+					break;
+				case 7:
+					System.out.println("Depósito\n\n");
+
+					break;
+				case 8:
+					System.out.println("Transferência entre Contas\n\n");
+
+					break;
+				default:
+					System.out.println("\nOpção Inválida!\n");
+					break;
+			}
+		}
+	}
+
+	}
+
